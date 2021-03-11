5b8ff9c657f82b0200574630fb982b09864c6522
Stanislav Zemlyakov <redtom@yandex.ru>
2021-03-11 11:09:33 +0300

    rcd-review_and_proposals: clean-up

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.31"
-//    kotlin("kapt")
     id("org.jetbrains.kotlin.kapt") version "1.4.31"
     application
 }
